1bebb99da235d31e14354a886316317f14235c34
XiaLiangBo <xialiangbo123@gmail.com>
2015-01-15 13:12:22 +0800

    add Contact file

---
+++ src/main/java/org/projectmvc/entity/Contact.java
@@ -0,0 +1,23 @@
+package org.projectmvc.entity;
+
+public class Contact extends BaseEntity<Long>{
+    private  int id;
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
